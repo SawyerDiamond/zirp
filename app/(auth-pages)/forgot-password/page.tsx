10940697312f2e0b1c3843ede24a41d51a099b2e
Sawyer Diamond <sawyerrdiamond@gmail.com>
2024-12-22 00:58:30 -0500

    Fixed reset password errors.

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -1,43 +1,13 @@
-"use client";
-import { forgotPasswordAction } from "@/app/actions";
-import { FormMessage, Message } from "@/components/form-message";
-import { SubmitButton } from "@/components/SubmitButton";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import Link from "next/link";
+import ForgotPasswordForm from "../../../components/ForgotPasswordForm";
+import { Message } from "@/components/form-message";
 
 export default async function ForgotPassword(props: {
   searchParams: Promise<Message>;
 }) {
-  const handleSubmit = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    const form = event.currentTarget.form;
-
-    if (form) {
-      const formData = new FormData(form);
-      await forgotPasswordAction(formData);
-    }
-  };
   const searchParams = await props.searchParams;
   return (
-    <>
-      <form className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64 mx-auto justify-center align-middle">
-        <div>
-          <h1 className="text-2xl font-medium">Reset Password</h1>
-          <p className="text-sm text-secondary-foreground">
-            Already have an account?
-            <Link className="text-primary underline" href="/sign-in">
-              Sign in
-            </Link>
-          </p>
-        </div>
-        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-          <Label htmlFor="email">Email</Label>
-          <Input name="email" placeholder="you@example.com" required />
-          <SubmitButton onClick={handleSubmit}>Reset Password</SubmitButton>
-          <FormMessage message={searchParams} />
-        </div>
-      </form>
-    </>
+    <div className="flex items-center justify-center h-screen">
+      <ForgotPasswordForm searchParams={searchParams} />
+    </div>
   );
 }
---
+++ components/ForgotPasswordForm.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { forgotPasswordAction } from "@/app/actions";
+import { FormMessage, Message } from "@/components/form-message";
+import { SubmitButton } from "@/components/SubmitButton";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function ForgotPasswordForm({
+  searchParams,
+}: {
+  searchParams: Message;
+}) {
+  const [message, setMessage] = useState(searchParams);
+
+  const handleSubmit = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget.form;
+
+    if (form) {
+      const formData = new FormData(form);
+      await forgotPasswordAction(formData);
+    }
+  };
+
+  return (
+    <form className="flex-1 flex flex-col gap- text-foreground w-96  mx-auto justify-center align-middle">
+      <div>
+        <h1 className="text-3xl font-medium">Reset Password</h1>
+      </div>
+      <div className="flex flex-col gap-2 [&>input]:mb-3 mt-3">
+        <Label htmlFor="email">Email</Label>
+        <Input name="email" placeholder="you@example.com" required />
+        <SubmitButton onClick={handleSubmit}>Reset Password</SubmitButton>
+        <FormMessage message={message} />
+      </div>
+    </form>
+  );
+}
