ec019d8fb310e75e93184ca47b68f391edad16a2
Sawyer Diamond <sawyerrdiamond@gmail.com>
2024-12-22 17:32:58 -0500

    Fixed mistake in client.

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,11 +1,13 @@
-// filepath: /Users/sawyer/Documents/Code/jobsite/utils/supabase/client.ts
-
 import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
   process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("supabaseUrl and supabaseAnonKey must be provided.");
+}
+
 export const createClient = () => {
-  return createSupabaseClient(supabaseUrl!, supabaseAnonKey);
+  return createSupabaseClient(supabaseUrl, supabaseAnonKey);
 };
